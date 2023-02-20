ee1dc16df2cca612687b2d660e3e74ebfcde8cf1
phnaharris <phnanh.harris@gmail.com>
2023-02-20 20:04:54 +0700

    fix(migration): complete migration, can continue to work

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,25 @@
 import rss from "@astrojs/rss";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
+import { getCollection } from "astro:content";
 
-export const get = () =>
-  rss({
+export async function get() {
+  const blog = (await getCollection("blog")).sort(
+    (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+  );
+  return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: import.meta.glob("./blog/**/*.md"),
+    // ex. use your stylesheet from "public/rss/styles.xsl"
+    stylesheet: "/rss/styles.xsl",
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      customData: post.data.customData,
+      // Compute RSS link from post `slug`
+      // This example assumes all posts are rendered as `/blog/[slug]` routes
+      link: `/blog/${post.slug}/`,
+    })),
   });
+}
